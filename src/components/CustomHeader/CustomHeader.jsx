d6756705ca0afbbf2ff1664ef4caae294302e604
feeerr95 <fgalvarez@fi.uba.ar>
2023-04-02 17:06:10 -0300

    arreglo el header para mobile

--- src/components/CustomHeader/CustomHeader.jsx
+++ src/components/CustomHeader/CustomHeader.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import './CustomHeaderStyles.css'
 import { Burger, Header, MediaQuery, Text } from '@mantine/core'
 
-const CustomHeader = ({ opened, setOpened }) => {
-  const handleOpen = () => {
-    setOpened((open) => !open)
-  }
-
+const CustomHeader = ({ opened, handleBurguerOpened }) => {
   return (
     <Header height={{ base: 50, md: 70 }} p="md">
       <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
         <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-          <Burger opened={opened} onClick={handleOpen} size="sm" mr="xl" />
+          <Burger
+            opened={opened}
+            onClick={handleBurguerOpened}
+            size="sm"
+            mr="xl"
+          />
         </MediaQuery>
 
         <Text>Application header</Text>
--- src/screens/DashboardScreen/DashboardScreen.jsx
+++ src/screens/DashboardScreen/DashboardScreen.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './DashboardScreenStyles.css'
 import { AppShell } from '@mantine/core'
 import { Route, Routes } from 'react-router'
+import { useMediaQuery } from '@mantine/hooks'
 import CustomNavbar from '../../components/CustomNavbar/CustomNavbar'
 import CustomHeader from '../../components/CustomHeader/CustomHeader'
 import CreateEventScreen from '../CreateEventScreen/CreateEventScreen'
@@ -9,15 +10,30 @@ import MyEventsScreen from '../MyEventsScreen/MyEventsScreen'
 import MetricsScreen from '../Metrics/MetricsScreen'
 
 const DashboardScreen = () => {
-  const [opened, setOpened] = useState(false)
+  const [openNavbar, setOpenNavbar] = useState(false)
+  const mobileScreen = useMediaQuery('(max-width: 48em)')
+  const [layout, setLayout] = useState('default')
+
+  useEffect(() => {
+    setLayout(mobileScreen ? 'default' : 'alt')
+  }, [mobileScreen])
+
+  const toggleOpenNavbarStatus = () => {
+    setOpenNavbar(!openNavbar)
+  }
 
   return (
     <AppShell
       navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
-      layout="alt"
-      navbar={<CustomNavbar opened={opened} />}
-      header={<CustomHeader />}
+      layout={layout}
+      navbar={<CustomNavbar opened={openNavbar} />}
+      header={
+        <CustomHeader
+          opened={openNavbar}
+          handleBurguerOpened={toggleOpenNavbarStatus}
+        />
+      }
     >
       <Routes>
         <Route path="createEvent" element={<CreateEventScreen />} />
