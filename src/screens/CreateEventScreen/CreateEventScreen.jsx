9f0de814fab243be5bcec7ec89e98c93bdfa55e7
feeerr95 <fgalvarez@fi.uba.ar>
2023-04-02 16:17:28 -0300

    creo la página de crear evento

---
+++ src/screens/CreateEventScreen/CreateEventScreen.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './CreateEventScreenStyles.css'
+
+const CreateEventScreen = () => {
+  return <div className="container">CreateEventScreen</div>
+}
+
+export default CreateEventScreen
