24be2c3ad3db4439437e45843b4d7127e143cbf9
feeerr95 <fgalvarez@fi.uba.ar>
2023-04-05 08:21:35 -0300

    add title input

--- src/screens/CreateEventScreen/CreateEventScreen.jsx
+++ src/screens/CreateEventScreen/CreateEventScreen.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import './CreateEventScreenStyles.css'
+import { useForm } from '@mantine/form'
+import { Button, Group, Select, TextInput } from '@mantine/core'
+import CustomRichTextEditor from '../../components/CustomRichTextEditor/CustomRichTextEditor'
 
 const CreateEventScreen = () => {
-  return <div className="container">CreateEventScreen</div>
+  const form = useForm({
+    initialValues: {
+      title: '',
+      type: '',
+      startDate: '',
+      description: '',
+    },
+  })
+
+  console.log(form.values)
+
+  return (
+    <div className="container">
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <TextInput
+          withAsterisk
+          label="Título"
+          placeholder="Ingrese el título del evento"
+          {...form.getInputProps('title')}
+        />
+
+        <Group position="right" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+      </form>
+    </div>
+  )
 }
 
 export default CreateEventScreen
