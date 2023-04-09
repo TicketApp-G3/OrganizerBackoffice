adf567a67811a56054ba6b298aa60d3f3197b55b
feeerr95 <feeerr95@gmail.com>
2023-04-09 20:19:00 -0300

    Ta 53/events visualization (#14)
    * agrego los formatters
    
    * agrego el endpoint para ver los eventos
    
    * Agrego las cards de los eventos
    
    * styleo las cards de mis eventos
    
    ---------
    
    Co-authored-by: feeerr95 <fgalvarez@fi.uba.ar>

--- src/screens/MyEventsScreen/MyEventsScreen.jsx
+++ src/screens/MyEventsScreen/MyEventsScreen.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './MyEventsScreenStyles.css'
+import { SimpleGrid, Space, Title } from '@mantine/core'
+import apiProvider from '../../api/apiProvider'
+import EventCard from '../../components/EventCard/EventCard'
 
 const MyEventsScreen = () => {
-  return <div className="container">MyEventsScreen</div>
+  const [events, setEvents] = useState([])
+
+  useEffect(() => {
+    apiProvider().getMyEvents({
+      userId: 1,
+      onSuccess: (response) => setEvents(response),
+      onFailure: (response) => console.log(response),
+    })
+  }, [])
+
+  return (
+    <>
+      <Title>Mis Eventos</Title>
+      <Space h={24} />
+      <SimpleGrid
+        cols={3}
+        breakpoints={[
+          { maxWidth: 'lg', cols: 3, spacing: 'lg' },
+          { maxWidth: 'md', cols: 2, spacing: 'md' },
+          { maxWidth: 'sm', cols: 2, spacing: 'sm' },
+          { maxWidth: 'xs', cols: 1, spacing: 'xs' },
+        ]}
+      >
+        {events.map((event) => (
+          <EventCard key={event.id} event={event} />
+        ))}
+      </SimpleGrid>
+    </>
+  )
 }
 
 export default MyEventsScreen
