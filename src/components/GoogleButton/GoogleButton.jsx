71652cec548909b3f0b394b19a603164fad14cf2
feeerr95 <fgalvarez@fi.uba.ar>
2023-04-29 19:04:41 -0300

    agrego el boton de google

---
+++ src/components/GoogleButton/GoogleButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from '@mantine/core'
+import './GoogleButtonStyles.css'
+
+const GoogleButton = () => {
+  return <Button className="buttonContainer">Google +</Button>
+}
+
+export default GoogleButton
