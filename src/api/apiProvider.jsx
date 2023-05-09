cd235a5e1d7aca91ac351b1a356474a966fde347
Fer Alvarez <fgalvarez@fi.uba.ar>
2023-05-09 20:45:32 -0300

    change bff port to 8082

--- src/api/apiProvider.jsx
+++ src/api/apiProvider.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiProvider = () => {
-  const baseURL = 'http://localhost:8081'
+  const baseURL = 'http://localhost:8082'
 
   const request = async ({
     method,
