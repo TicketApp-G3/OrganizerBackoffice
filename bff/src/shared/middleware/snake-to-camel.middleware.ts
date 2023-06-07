1441bb4b594d5d37a2e3b41219b14bc4d6bf3ce1
Hugo Larrea <hugo.larrea@creativecoefficient.net>
2023-06-07 20:09:25 -0300

    name fix

--- bff/src/shared/middleware/snake-to-camel.middleware.ts
+++ bff/src/shared/middleware/snake-to-camel.middleware.ts
@@ -1,9 +1,9 @@
-import { RequestHandler } from 'express';
-import camelcaseKeys from 'camelcase-keys';
+import { RequestHandler } from 'express'
+import camelcaseKeys from 'camelcase-keys'
 
 export function snakeToCamelcaseKeys(): RequestHandler {
   return (req, _, next) => {
-    req.body = camelcaseKeys(req.body, { deep: true });
-    next();
-  };
+    req.body = camelcaseKeys(req.body, { deep: true })
+    next()
+  }
 }
